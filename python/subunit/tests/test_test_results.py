4ea90f04f1b71d01eed6721c36f99a781d24d8cf
Robert Collins <robertc@robertcollins.net>
2012-05-08 07:36:05 +1200

    * Python3 support regressed in trunk.   (Arfrever Frehtes Taifersar Arahesis, #987514)

--- python/subunit/tests/test_test_results.py
+++ python/subunit/tests/test_test_results.py
@@ -371,7 +371,10 @@ class TestByTestResultTests(testtools.TestCase):
         super(TestByTestResultTests, self).setUp()
         self.log = []
         self.result = subunit.test_results.TestByTestResult(self.on_test)
-        self.result._now = iter(range(5)).next
+        if sys.version_info >= (3, 0):
+            self.result._now = iter(range(5)).__next__
+        else:
+            self.result._now = iter(range(5)).next
 
     def assertCalled(self, **kwargs):
         defaults = {
@@ -527,7 +530,10 @@ class TestCsvResult(testtools.TestCase):
     def test_csv_output(self):
         stream = StringIO()
         result = subunit.test_results.CsvResult(stream)
-        result._now = iter(range(5)).next
+        if sys.version_info >= (3, 0):
+            result._now = iter(range(5)).__next__
+        else:
+            result._now = iter(range(5)).next
         result.startTestRun()
         result.startTest(self)
         result.addSuccess(self)
