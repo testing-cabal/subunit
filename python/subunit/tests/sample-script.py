a629de4fbf04b62361e67a1e04e29474ca18d2b0
Jelmer Vernooĳ <jelmer@jelmer.uk>
2022-01-25 18:13:25 +0000

    Explicitly invoke python3.
    That's the only major version subunit supports, and some platforms still ship with /usr/bin/python==python2

--- python/subunit/tests/sample-script.py
+++ python/subunit/tests/sample-script.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import sys
 if sys.platform == "win32":
     import msvcrt, os
--- python/subunit/tests/sample-two-script.py
+++ python/subunit/tests/sample-two-script.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import sys
 print("test old mcdonald")
 print("success old mcdonald")
