00d551053d6a6b4fb5a57100161a8fe97acd460a
Jelmer Vernooĳ <jelmer@jelmer.uk>
2025-11-10 10:54:38 +0000

    release 1.4.5

--- python/subunit/__init__.py
+++ python/subunit/__init__.py
@@ -144,7 +144,7 @@ from subunit.v2 import ByteStreamToStreamResult, StreamResultToBytes
 # If the releaselevel is 'final', then the tarball will be major.minor.micro.
 # Otherwise it is major.minor.micro~$(revno).
 
-__version__ = (1, 4, 4, "final", 0)
+__version__ = (1, 4, 5, "final", 0)
 
 version_string = ".".join(map(str, __version__[:3]))
 
